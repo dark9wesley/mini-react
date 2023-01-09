09c8e9519de62e180235e44607e07f589fade6a6
pengyw97 <pengyw97@gmail.com>
2023-01-09 15:46:30 +0800

    feat: mount hook 架构实现

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,36 @@
+import internals from 'shared/internals'
 import { FiberNode } from './fiber'
 
+interface Hook {
+	memorizedState: any
+	updateQueue: unknown
+	next: Hook | null
+}
+
+let currentlyRenderingFiber: FiberNode | null = null
+const workInProgressHook: Hook | null = null
+const { currentDispatcher } = internals
+
 export function renderWithHooks(wip: FiberNode) {
+	// 赋值操作
+	currentlyRenderingFiber = wip
+	wip.memorizedState = null
+
+	const current = wip.alternate
+
+	if (current !== null) {
+		// update
+	} else {
+		// mount
+		// currentDispatcher.current = {}
+	}
+
 	const Component = wip.type
 	const props = wip.pendingProps
 	const children = Component(props)
+
+	// 重置操作
+	currentlyRenderingFiber = null
 
 	return children
 }
