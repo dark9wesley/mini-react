eedc3fabde71d895ce59a90cd2f1548626f29425
pengyw97 <pengyw97@gmail.com>
2023-01-11 20:23:07 +0800

    feat: beginWork增加 update 流程

--- packages/react-reconciler/src/childFibers.ts
+++ packages/react-reconciler/src/childFibers.ts
@@ -1,15 +1,62 @@
-import { createFiberFromElement, FiberNode } from './fiber'
-import { ReactElement } from 'shared/ReactTypes'
+import { createFiberFromElement, FiberNode, createWorkInProgess } from './fiber'
+import { Props, ReactElement } from 'shared/ReactTypes'
 import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols'
 import { HostText } from './workTags'
-import { Placement } from './fiberFlags'
+import { ChildDeletion, Placement } from './fiberFlags'
 
 function ChildReconciler(shouldTrackEffects: boolean) {
+	function deleteChild(returnFiber: FiberNode, childToDelete: FiberNode) {
+		if (!shouldTrackEffects) {
+			return
+		}
+		const deletions = returnFiber.deletions
+		if (deletions === null) {
+			returnFiber.deletions = [childToDelete]
+			returnFiber.flags |= ChildDeletion
+		} else {
+			deletions.push(childToDelete)
+		}
+	}
+
+	function useFiber(fiber: FiberNode, pengdingProps: Props) {
+		const clone = createWorkInProgess(fiber, pengdingProps)
+		clone.index = 0
+		clone.sibling = null
+		return clone
+	}
+
 	function reconcileSingleElement(
 		returnFiber: FiberNode,
 		currentFiber: FiberNode | null,
 		element: ReactElement
 	) {
+		const key = element.key
+		work: if (currentFiber !== null) {
+			// update
+			if (currentFiber.key === key) {
+				// key相同
+				if (element.$$typeof === REACT_ELEMENT_TYPE) {
+					if (currentFiber.type === element.type) {
+						const existing = useFiber(currentFiber, element.props)
+						existing.return = returnFiber
+						return existing
+					}
+					// 删掉旧的
+					deleteChild(returnFiber, currentFiber)
+					break work
+				} else {
+					if (__DEV__) {
+						console.warn('还未实现的类型', element)
+						break work
+					}
+				}
+			} else {
+				// 删掉旧的
+				deleteChild(returnFiber, currentFiber)
+				break work
+			}
+		}
+
 		const fiber = createFiberFromElement(element)
 		fiber.return = returnFiber
 		return fiber
@@ -20,6 +67,16 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 		currentFiber: FiberNode | null,
 		content: string | number
 	) {
+		if (currentFiber !== null) {
+			// update
+			if (currentFiber.tag === HostText) {
+				const existing = useFiber(currentFiber, { content })
+				existing.return = returnFiber
+				return existing
+			}
+			deleteChild(returnFiber, currentFiber)
+		}
+
 		const fiber = new FiberNode(HostText, { content }, null)
 		fiber.return = returnFiber
 		return fiber
@@ -57,6 +114,11 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 			return placeSingleChild(
 				reconcileSingleTextNode(returnFiber, currentFiber, newChild)
 			)
+		}
+
+		if (currentFiber !== null) {
+			// 兜底情况
+			deleteChild(returnFiber, currentFiber)
 		}
 
 		if (__DEV__) {
