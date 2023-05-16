95e017ce9e5acc85d4bf881b97d5bf143c960e07
pengyw97 <877285030@qq.com>
2023-05-16 23:47:46 +0800

    feat: 初步实现react.createElement方法

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,5 +1,12 @@
 import { REACT_ELEMENT } from 'shared/ReactSymbols'
-import { Type, Key, Ref, Props, ReactElement } from 'shared/ReactTypes'
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElement,
+	ElementType
+} from 'shared/ReactTypes'
 
 // ReactElement
 const ReactElement = (type: Type, key: Key, ref: Ref, props: Props) => {
@@ -14,3 +21,45 @@ const ReactElement = (type: Type, key: Key, ref: Ref, props: Props) => {
 
 	return element
 }
+
+export const jsx = (
+	type: ElementType,
+	config: any,
+	...maybeChildren: any[]
+) => {
+	let key: Key = null
+	let ref: Ref = null
+	const props: Props = {}
+
+	for (const prop in config) {
+		const val = config[prop]
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = val + ''
+			}
+			continue
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val
+			}
+			continue
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0]
+		} else {
+			props.children = maybeChildren
+		}
+	}
+
+	return ReactElement(type, key, ref, props)
+}
+
+export const jsxDEV = jsx
