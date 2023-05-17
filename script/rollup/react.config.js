f68b5f15c582464a1705734052e4c3ef2bf02d1e
pengyw97 <877285030@qq.com>
2023-05-17 21:47:17 +0800

    feat: 配置rollup打包文件

---
+++ script/rollup/react.config.js
@@ -0,0 +1,18 @@
+import { getPackageJSON, resolvePkgPath } from './utils'
+
+const { name, module } = getPackageJSON('react')
+// react包路径
+const pkgPath = resolvePkgPath(name)
+// react产物路径
+const pkgDistPath = resolvePkgPath(name, true)
+
+export default [
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index,js`,
+			name: 'index.js',
+			format: 'umd'
+		}
+	}
+]
