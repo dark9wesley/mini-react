c3e03d37fa040c0a178a6e772068d8cecc7e4c74
pengyw97 <877285030@qq.com>
2023-05-17 22:14:31 +0800

    feat: react打包产物区分

--- scripts/rollup/react.config.js
+++ scripts/rollup/react.config.js
@@ -7,6 +7,7 @@ const pkgPath = resolvePkgPath(name)
 const pkgDistPath = resolvePkgPath(name, true)
 
 export default [
+	// react
 	{
 		input: `${pkgPath}/${module}`,
 		output: {
@@ -14,6 +15,25 @@ export default [
 			name: 'index.js',
 			format: 'umd'
 		},
+		plugins: getBaseRollupPlugins()
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
 		plugins: getBaseRollupPlugins()
 	}
 ]
