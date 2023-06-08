b1dd9885158784af7dc6f6f6ccefe4598455e692
pengyw97 <877285030@qq.com>
2023-06-08 21:04:46 +0800

    feat: 引入@rollup/plugin-replace

--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -37,7 +37,9 @@ function renderRoot(root: FiberRootNode) {
 			workLoop()
 			break
 		} catch (e) {
-			console.warn('workloop发生错误', e)
+			if (__DEV__) {
+				console.warn('workloop发生错误', e)
+			}
 			workInProgress = null
 		}
 	} while (true)
