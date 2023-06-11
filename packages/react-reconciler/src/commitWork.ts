aeb976c7144a18b08ecf2448898b64f495b86a4e
pengyw97 <877285030@qq.com>
2023-06-11 21:31:03 +0800

    feat: 找到flags对应的fiber并执行placement操作

--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -1,5 +1,5 @@
 import { FiberNode } from './fiber'
-import { MutationMask, NoFlags } from './fiberFlags'
+import { MutationMask, NoFlags, Placement } from './fiberFlags'
 
 let nextEffect: FiberNode | null = null
 
@@ -17,7 +17,7 @@ export const commitMutationEffects = (finishedWork: FiberNode) => {
 		} else {
 			// 向上遍历
 			up: while (nextEffect !== null) {
-				// xxx();
+				commitMutationEffectsOnFiber(nextEffect)
 				const sibling: FiberNode | null = nextEffect.sibling
 				if (sibling !== null) {
 					nextEffect = sibling
@@ -26,5 +26,26 @@ export const commitMutationEffects = (finishedWork: FiberNode) => {
 				nextEffect = nextEffect.return
 			}
 		}
+	}
+}
+
+const commitMutationEffectsOnFiber = (finishedWork: FiberNode) => {
+	const flags = finishedWork.flags
+
+	if ((flags & Placement) !== NoFlags) {
+		commitPlacement(finishedWork)
+		finishedWork.flags &= ~Placement
+	}
+}
+
+/**
+ * 执行插入操作
+ * 1. 向上找到被插入的父节点
+ * 2. 向下找到要插入的节点
+ * @param {FiberNode} finishedWork
+ */
+const commitPlacement = (finishedWork: FiberNode) => {
+	if (__DEV__) {
+		console.warn('执行Placement操作')
 	}
 }
