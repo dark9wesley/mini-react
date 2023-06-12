c56a4d53ada02ab0829f73984d28447654d8a604
pengyw97 <877285030@qq.com>
2023-06-13 00:43:31 +0800

    feat: 引入vite调试源码

---
+++ demos/test-fc/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <div>111111</div>
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<App />
+)
