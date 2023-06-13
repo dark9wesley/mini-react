939f6cd933ff8d250a2cc4b5ba6b83d15c14e6b7
pengyw97 <877285030@qq.com>
2023-06-13 23:22:46 +0800

    feat: 定义currentlyRenderingFiber保存正在渲染的函数组件fiber

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,8 +1,14 @@
 import { FiberNode } from './fiber'
 
+let currentlyRenderingFiber: FiberNode | null = null
+
 export function renderWithHooks(wip: FiberNode) {
+	// 赋值
+	currentlyRenderingFiber = wip
 	const Component = wip.type
 	const props = wip.pendingProps
 	const children = Component(props)
+	// 重置
+	currentlyRenderingFiber = null
 	return children
 }
