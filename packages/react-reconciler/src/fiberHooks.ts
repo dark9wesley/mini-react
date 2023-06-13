c31ff70c84353c4ff619837b3f57faa38cfd1e71
pengyw97 <877285030@qq.com>
2023-06-13 23:33:41 +0800

    feat: 实现渲染FC时处理数据

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,10 +1,32 @@
+import internals from 'shared/internals'
 import { FiberNode } from './fiber'
 
 let currentlyRenderingFiber: FiberNode | null = null
 
+const workInProgressHook: Hook | null = null
+
+interface Hook {
+	memorizedState: any
+	updateQueue: unknown
+	next: Hook | null
+}
+
+const { currentDispatcher } = internals
+
 export function renderWithHooks(wip: FiberNode) {
 	// 赋值
 	currentlyRenderingFiber = wip
+	wip.memorizeState = null
+
+	const current = wip.alternate
+
+	if (current !== null) {
+		//update
+	} else {
+		//mount时的hook实现
+		// currentDispatcher.current =
+	}
+
 	const Component = wip.type
 	const props = wip.pendingProps
 	const children = Component(props)
