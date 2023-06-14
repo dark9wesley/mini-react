c5f903a31042678f407dbb69f59b66cecd276bdc
pengyw97 <877285030@qq.com>
2023-06-14 22:54:30 +0800

    feat: 实现mount useState

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,14 @@
 import internals from 'shared/internals'
 import { FiberNode } from './fiber'
+import { Dispatch, Dispatcher } from 'react/src/currentDispatcher'
+import { UpdateQueue, createUpdateQueue } from './updateQueue'
+import { createUpdate } from './updateQueue'
+import { enqueueUpdate } from './updateQueue'
+import { scheduleUpdateOnFiber } from './workLoop'
+import { Action } from 'shared/ReactTypes'
 
 let currentlyRenderingFiber: FiberNode | null = null
-
-const workInProgressHook: Hook | null = null
+let workInProgressHook: Hook | null = null
 
 interface Hook {
 	memorizedState: any
@@ -24,7 +29,7 @@ export function renderWithHooks(wip: FiberNode) {
 		//update
 	} else {
 		//mount时的hook实现
-		// currentDispatcher.current =
+		currentDispatcher.current = HookDispatcherOnMount
 	}
 
 	const Component = wip.type
@@ -33,4 +38,66 @@ export function renderWithHooks(wip: FiberNode) {
 	// 重置
 	currentlyRenderingFiber = null
 	return children
+}
+
+const HookDispatcherOnMount: Dispatcher = {
+	useState: mountState
+}
+
+function mountState<State>(
+	initialState: (() => State) | State
+): [State, Dispatch<State>] {
+	// 找到当前useState对应的hook数据
+	const hook = mountWorkInProgressHook()
+
+	let memorizedState
+	if (initialState instanceof Function) {
+		memorizedState = initialState()
+	} else {
+		memorizedState = initialState
+	}
+
+	const queue = createUpdateQueue<State>()
+	hook.updateQueue = queue
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue)
+	queue.dispatch = dispatch
+
+	return [memorizedState, dispatch]
+}
+
+function dispatchSetState<State>(
+	fiber: FiberNode,
+	updateQueue: UpdateQueue<State>,
+	action: Action<State>
+) {
+	const update = createUpdate(action)
+	enqueueUpdate(updateQueue, update)
+	scheduleUpdateOnFiber(fiber)
+}
+
+function mountWorkInProgressHook(): Hook {
+	const hook: Hook = {
+		memorizedState: null,
+		updateQueue: null,
+		next: null
+	}
+
+	if (workInProgressHook === null) {
+		// mount时，且当前为第一个hook
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用hook')
+		} else {
+			workInProgressHook = hook
+			currentlyRenderingFiber.memorizeState = workInProgressHook
+		}
+	} else {
+		// mount时，后续的hook
+		workInProgressHook.next = hook
+		workInProgressHook = hook
+	}
+
+	return workInProgressHook
 }
