52c64ff412ca105fa1c1480b4144554cf335272b
pengyw97 <pengyw97@gmail.com>
2023-06-15 19:06:49 +0800

    feat: 调整代码通过测试用例

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,5 +1,5 @@
 import { Dispatcher, resolveDispatcher } from './src/currentDispatcher'
-import { jsxDEV } from './src/jsx'
+import { jsx } from './src/jsx'
 import currentDispatcher from './src/currentDispatcher'
 
 export const useState: Dispatcher['useState'] = (initialState: any) => {
@@ -12,7 +12,9 @@ export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
 	currentDispatcher
 }
 
-export default {
-	version: '0.0.0',
-	createElement: jsxDEV
-}
+export const version = '0.0.0'
+
+// TODO 根据使用环境导出jsx/jsxDEV
+export const createElement = jsx
+
+export { isValidElement } from './src/jsx'
