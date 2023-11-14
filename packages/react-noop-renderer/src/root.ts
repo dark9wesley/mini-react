3a60c628b5814b13fdc56287df6d6f0a5fb82da2
pengyw97 <pengyw97@gmail.com>
2023-11-14 17:48:26 +0800

    feat: noop-render增加测试方法

--- packages/react-noop-renderer/src/root.ts
+++ packages/react-noop-renderer/src/root.ts
@@ -4,6 +4,7 @@ import {
 } from 'react-reconciler/src/fiberReconciler'
 import { Container, Instance } from './hostConfig'
 import { ReactElement } from 'shared/ReactTypes'
+import { REACT_ELEMENT, REACT_FRAGMENT } from 'shared/ReactSymbols'
 
 let idCounter = 0
 
@@ -24,12 +25,83 @@ export function createRoot() {
 		return null
 	}
 
+	function getChildrenAsJSX(root: Container) {
+		const children = childToJSX(getChildren(root))
+
+		if (Array.isArray(children)) {
+			return {
+				$$typeof: REACT_ELEMENT,
+				type: REACT_FRAGMENT,
+				key: null,
+				ref: null,
+				props: { children },
+				__mark: 'Wesley'
+			}
+		}
+
+		return children
+	}
+
+	function childToJSX(child: any): any {
+		if (typeof child === 'string' || typeof child === 'number') {
+			return child
+		}
+
+		if (Array.isArray(child)) {
+			if (child.length === 0) {
+				return null
+			}
+
+			if (child.length === 1) {
+				return childToJSX(child[0])
+			}
+
+			const children = child.map(childToJSX)
+
+			if (
+				child.every(
+					(child) => typeof child === 'string' || typeof child === 'number'
+				)
+			) {
+				return children.join('')
+			}
+
+			return children
+		}
+
+		// Instance
+		if (Array.isArray(child.children)) {
+			const instance: Instance = child
+			const children = childToJSX(instance.children)
+			const props = instance.props
+
+			if (children !== null) {
+				props.children = children
+			}
+
+			return {
+				$$typeof: REACT_ELEMENT,
+				type: instance.type,
+				key: null,
+				ref: null,
+				props,
+				__mark: 'Wesley'
+			}
+		}
+
+		// TextInstance
+		return child.text
+	}
+
 	return {
 		render(element: ReactElement) {
 			return UpdateContainer(element, root)
 		},
 		getChildren() {
 			return getChildren(container)
+		},
+		getChildrenAsJSX() {
+			return getChildrenAsJSX(container)
 		}
 	}
 }
